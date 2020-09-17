64461ee36dede1fa54c725b86e71a7adf6ddab66
josh-rosairo <32214480+josh-rosairo@users.noreply.github.com>
2020-09-17 17:41:42 +1200

    Progress commit - Documentation
    - Added Documentation for intended functionality, specifically outlining the business logic,
    - This commit will not compile as is.

--- src/components/reducers/shoppingCartReducer.tsx
+++ src/components/reducers/shoppingCartReducer.tsx
@@ -11,7 +11,19 @@ const initState = {
     total: 0
 }
 
-const shoppingCartReducer= (state = initState)=>{
-    return state;
+const shoppingCartReducer= (state = initState, action)=>{
+    if(action.type === ADD_TO_CART){ //%TODO action types.
+        // find clicked on product in our state's list of product constants.
+        let addedProduct = state.products.find(product=> product.name === action.product.name && product.price === action.product.price)
+
+        //check if its already in cart, in which case we increase the quantity total by 1
+
+        //if not, then we add the product to cart
+
+        //regardless we recalculate price total > apply changes to state > return.
+    }
+    else {
+        return state;
+    }
 }
 export default shoppingCartReducer;
