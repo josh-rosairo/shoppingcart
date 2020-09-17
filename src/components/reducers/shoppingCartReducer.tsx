521f2c80dcf6d7a0e8a1802eedcab8c3ad4dae77
josh-rosairo <32214480+josh-rosairo@users.noreply.github.com>
2020-09-17 17:56:55 +1200

    Last Commit - Out of Time
    - Worked on some more of the reducer functionality.
    - Changed constants to also include quantity field as a placeholder for cart calculations. (this was purely for state and may need to be refactored.)
    - For compilation sake, have left those additions commented out.
    - Also added %TODO comments to show what steps I was considering needed to be done (for additions to work.)

--- src/components/reducers/shoppingCartReducer.tsx
+++ src/components/reducers/shoppingCartReducer.tsx
@@ -1,4 +1,4 @@
-import { products } from '../../constants/index';
+import {Product, products} from '../../constants/index';
 
 /**
  *
@@ -7,23 +7,32 @@ import { products } from '../../constants/index';
 
 const initState = {
     products: products,
-    cartProducts: [],
+    cartProducts: [], //%TODO set type as Product
     total: 0
 }
 
-const shoppingCartReducer= (state = initState, action)=>{
+const shoppingCartReducer= (state = initState, action: any)=>{
+    /***
     if(action.type === ADD_TO_CART){ //%TODO action types.
-        // find clicked on product in our state's list of product constants.
+        // find clicked on product in our state's list of product constants and save said product for use further down.
         let addedProduct = state.products.find(product=> product.name === action.product.name && product.price === action.product.price)
 
         //check if its already in cart, in which case we increase the quantity total by 1
+        let productAlreadyCarted = state.cartProducts.find(product=> product.name === action.product.name && product.price === action.product.price)
+        if (productAlreadyCarted) {
+            addedProduct.quantity += 1
+            return {
+                ...state,
+                total: state.total + addedProduct.price
+            }
+        }
 
         //if not, then we add the product to cart
 
         //regardless we recalculate price total > apply changes to state > return.
     }
-    else {
-        return state;
-    }
+    ***/
+
+    return state;
 }
 export default shoppingCartReducer;
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,26 +1,32 @@
 export const products = [
   {
     name: 'Sledgehammer',
-    price: 125.76
+    price: 125.76,
+    quantity: 0
   },
   {
     name: 'Axe',
-    price: 190.51
+    price: 190.51,
+    quantity: 0
   },
   {
     name: 'Bandsaw',
-    price: 562.14
+    price: 562.14,
+    quantity: 0
   },
   {
     name: 'Chisel',
-    price: 13.9
+    price: 13.9,
+    quantity: 0
   },
   {
     name: 'Hacksaw',
-    price: 19.45
+    price: 19.45,
+    quantity: 0
   }
 ];
 export interface Product {
   name: string,
-  price: number
+  price: number,
+  quantity: 0
 }
